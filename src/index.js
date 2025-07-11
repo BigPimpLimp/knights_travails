c7a435d835c090961af772faa095256692353baf
bigpimplimp <levi.jcecil@gmail.com>
2025-07-11 15:33:50 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './style.css';
 
 console.log('Connected');
+
+class Knight {
+  constructor() {
+      this.position = [0, 0];
+      this.visited = [];
+			this.movesCounter = 0;
+  }
+
+  currentPosition(x, y) {
+      this.position = [x, y];
+  }
+
+  knightMoves() {
+
+	}
+}
+
+
